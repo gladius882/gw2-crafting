7dbe57641ad41ec9e9d3c552440ba8223cc39275
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-11-18 15:05:32 +0100

    add missing disciplines in recipes seeder

--- prisma/seed/recipes.ts
+++ prisma/seed/recipes.ts
@@ -8,15 +8,29 @@ async function main() {
 
     const recipeIds = await listRecipes();
 
-    if(recipeIds === false) {
+    if (recipeIds === false) {
         console.log('Cant fetch recipes');
         return;
     }
 
+    let i = 0;
     for await (const recipe_id of recipeIds) {
-        
+
+        i = i + 1;
+
         const gw2Recipe = await getRecipe(recipe_id as number);
-        console.log(gw2Recipe);
+
+        const disciplines = [];
+
+        for await (const discipline_name of gw2Recipe.disciplines ?? []) {
+            const discipline = await prisma.discipline.upsert({
+                where: { name: discipline_name },
+                update: {},
+                create: { name: discipline_name }
+            })
+        }
+
+        console.log(`[${i} / ${recipeIds.length}][NEW RECIPE] ${gw2Recipe.chat_link}`);
         break;
     }
 
