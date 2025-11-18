3c18bd9e794ed6c50a8facce79fae99e1bff0b75
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-11-18 14:47:28 +0100

    get recipe server action

--- lib/recipes.ts
+++ lib/recipes.ts
@@ -5,3 +5,7 @@ import { gw2fetch } from "./client"
 export async function listRecipes() {
     return await gw2fetch('recipes');
 }
+
+export async function getRecipe(recipe_id: number) {
+    return await gw2fetch(`recipes/${recipe_id}`);
+}
