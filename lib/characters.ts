b1ffaf32ff0c4a9d980ba4990b0daf825aca0b32
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-11-19 12:42:20 +0100

    get character server action

--- lib/characters.ts
+++ lib/characters.ts
@@ -3,3 +3,7 @@ import { gw2fetch } from "./client";
 export async function listCharacters() {
     return await gw2fetch('characters');
 }
+
+export async function getCharacter(name: string) {
+    return await gw2fetch(`characters/${name}`)
+}
