363911e35bb9bbc50b2e27c03098471457e69568
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-11-19 13:46:20 +0100

    get bank materials server action

--- lib/materials.ts
+++ lib/materials.ts
@@ -1,13 +1,28 @@
 "use server"
 
+import { PrismaClient } from "@/prisma/prisma/generated/client";
 import { gw2fetch } from "./client";
 
 export async function listMaterialIds() {
-    
+
     return await gw2fetch('materials');
 }
 
 export async function getMaterial(id: number) {
 
-   return await gw2fetch(`/materials/${id}`)
+    return await gw2fetch(`/materials/${id}`)
+}
+
+export async function getBankMaterials() {
+    const prisma = new PrismaClient();
+
+    const bankItems = await prisma.bankMaterial.findMany({
+        include: {
+            item: true
+        },
+    })
+
+    prisma.$disconnect();
+
+    return bankItems;
 }
