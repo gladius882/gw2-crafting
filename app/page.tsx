332eaf762ae77b03fa0e5d03cc116447c281e2c9
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-11-19 13:46:31 +0100

    simple bank materials list

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
 "use server"
 
 import { getItem, listItems } from "@/lib/items";
-import { getMaterial, listMaterialIds } from "@/lib/materials";
+import { getBankMaterials, getMaterial, listMaterialIds } from "@/lib/materials";
 import { listRecipes } from "@/lib/recipes";
+import Image from "next/image";
 
 export default async function Home() {
 
-  const recipes = await listRecipes();
+  const bank = await getBankMaterials();
 
 
   return (
-    <div className="">
+    <div className="grid grid-cols-10 gap-3">
 
-      <div>
-        {JSON.stringify(recipes)}
-      </div>
+      {bank.map(b => {
+        return (
+          <div key={b.id} className="shadow-white shadow text-center flex flex-col items-center justify-center p-2">
+            <Image src={b.item.icon} alt="test" width={50} height={50} />
+            <div>{b.count}</div>
+          </div>
+        )
+      })}
 
       
     </div>
