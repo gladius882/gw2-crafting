5cca941159b4480656b2137c0c3af42a7411a977
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-11-19 13:29:42 +0100

    bank materials seeder

---
+++ prisma/seed/materials.ts
@@ -0,0 +1,44 @@
+import "dotenv/config"
+import { PrismaClient } from "../prisma/generated/client"
+import { listMaterials } from "@/lib/account";
+
+const prisma = new PrismaClient();
+
+async function main() {
+
+    const materials = await listMaterials();
+
+    await prisma.bankMaterial.updateMany({
+        data: { count: 0 }
+    })
+
+
+    for await (const material of materials) {
+        
+        const item = await prisma.item.findFirst({
+            where: { gw2_id: material.id }
+        })
+
+        if(!item) {
+            console.log(`[SKIP] Not found item with gw_id = ${material.id}`)
+            continue;
+        }
+
+        const result = await prisma.bankMaterial.upsert({
+            where: { item_id: item.id },
+            update: { count: material.count },
+            create: {
+                count: material.count,
+                item_id: item.id
+            }
+        })
+
+        console.log(`Updated count in materials bank for item ${item.name}`);
+    }
+
+}
+
+main()
+    .then(() => console.log('success'))
+    .catch(console.log)
+    .finally(() => prisma.$disconnect)
