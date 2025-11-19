437d7ed8e60216019f895354196ef2c5c1a1af6d
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-11-19 12:55:45 +0100

    list materials server action

--- lib/account.ts
+++ lib/account.ts
@@ -3,3 +3,7 @@ import { gw2fetch } from "./client";
 export async function getAccount() {
     return await gw2fetch('account');
 }
+
+export async function listMaterials() {
+    return await gw2fetch('account/materials');
+}
