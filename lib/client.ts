2a8f968a3a1d01daf2a0a13cfb1e06df0fc62535
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-11-19 12:27:41 +0100

    use api key in query params in gw2 client

--- lib/client.ts
+++ lib/client.ts
@@ -1,10 +1,5 @@
 export async function gw2fetch(endpoint: string) {
-    return fetch(`${process.env.API_BASE_URL}/v2/${endpoint}`, {
-        method: "GET",
-        headers: {
-            "Authentication": `Bearer ${process.env.API_KEY}`
-        }
-    })
+    return fetch(`${process.env.API_BASE_URL}/v2/${endpoint}?access_token=${process.env.API_KEY}`)
         .then(data => data.json())
         .catch((err) => {
             console.log(err);
