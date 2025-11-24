ab328595ee0920160fca3f4ab65a60926d59028e
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-11-24 11:16:24 +0100

    get wishlisted items server action

--- lib/items.ts
+++ lib/items.ts
@@ -1,6 +1,8 @@
 "use server"
 
+import "dotenv/config"
 import { gw2fetch } from "./client"
+import { PrismaClient } from "@/prisma/prisma/generated/client";
 
 export async function listItems() {
     return await gw2fetch('items');
@@ -8,4 +10,18 @@ export async function listItems() {
 
 export async function getItem(id: number) {
     return await gw2fetch(`items/${id}`)
+}
+
+export async function getWishlistedItems() {
+    const prisma = new PrismaClient();
+
+    try {
+        return await prisma.item.findMany({
+            where: { is_wishlised: true }
+        })
+    }
+    catch {}
+    finally {
+        prisma.$disconnect();
+    }
 }
