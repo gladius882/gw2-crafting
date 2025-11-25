285ff32e080bce8626f65b25e66ef17c70beb987
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-11-25 15:16:20 +0100

    load accounts and characters in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,41 @@
 "use server"
 
 import { getBankMaterials } from "@/lib/materials";
+import { PrismaClient } from "@/prisma/prisma/generated/client";
 import Image from "next/image";
 
 export default async function Home() {
 
-  const bank = await getBankMaterials();
+  const prisma = new PrismaClient();
+  const accounts = await prisma.account.findMany();
+  const characters = await prisma.character.findMany({
+    include: { race: true }
+  });
 
 
   return (
     <div className="grid grid-cols-2 gap-3">
 
-        
+      <div>Wybierz postać</div>
 
-      
+      {accounts.map(a => {
+        return (
+          <div key={a.id}>
+            {a.gw2_id}
+          </div>
+        )
+      })}
+
+
+      {characters.map(c => {
+        return (
+          <div key={c.id}>
+            {c.name}
+          </div>
+        )
+      })}
+
+
     </div>
   );
 }
