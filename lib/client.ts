38e16c07dda0e9f25d38caf20aa051292d5c25fe
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-11-25 14:15:46 +0100

    dotenv in gw2 client

--- lib/client.ts
+++ lib/client.ts
@@ -1,3 +1,5 @@
+import "dotenv/config"
+
 export async function gw2fetch(endpoint: string) {
     return fetch(`${process.env.API_BASE_URL}/v2/${endpoint}?access_token=${process.env.API_KEY}`)
         .then(data => data.json())
