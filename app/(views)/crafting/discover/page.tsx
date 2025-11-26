05adf503fbf0e6a8424c12e46344e2f94980bf01
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-11-26 09:52:31 +0100

    use discovery listing component

--- app/(views)/crafting/discover/page.tsx
+++ app/(views)/crafting/discover/page.tsx
@@ -1,6 +1,13 @@
 "use server"
 
+import DiscoveryRecipesListing from "@/components/DiscoveryRecipesListing";
+import { PrismaClient } from "@/prisma/prisma/generated/client";
+
 export default async function DiscoverPage() {
+
+    const prisma = new PrismaClient();
+    const disciplines = await prisma.discipline.findMany();
+    const rarities = await prisma.rarity.findMany();
 
     const res = await fetch('http://localhost:3000/api/crafting/discover');
     const toDiscover = await res.json();
@@ -10,38 +17,7 @@ export default async function DiscoverPage() {
 
             <h1 className="text-lg">Discover recipes</h1>
 
-            {toDiscover.map(item => {
-                return (
-                    <div key={item.id} className="flex gap-5 mb-5">
-                        <div>
-                            <img className="w-14 h-14" src={item.icon} alt={item.name} />
-                        </div>
-
-                        <div>
-                            <div>
-                                <a href={`/crafting/${item.id}`}>
-                                    <span style={{
-                                        color: item.rarity.color
-                                    }}>{item.name}</span> ({item.min_rating})
-                                </a>
-                            </div>
-
-                            <div className="flex gap-5">
-                                {item.disciplines.map(d => {
-                                    return (
-                                        <div key={d.discipline.id} className="flex items-center">
-                                            {d.discipline.icon && (
-                                                <img src={d.discipline.icon} alt={d.discipline.name} className="w-8 h-8" />
-                                            )}
-                                            {d.discipline.name}
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                        </div>
-                    </div>
-                )
-            })}
+            <DiscoveryRecipesListing disciplines={disciplines} rarities={rarities} />
         </div>
     )
 }
